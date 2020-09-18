7e714a448cc4ef7469f844e8cdce53d10db7ff1a
ccarlosf <2238513100@qq.com>
2020-09-18 14:29:24 +0800

    01 第一阶段 开源框架源码剖析 模块二 IoC容器设计实现及Spring源码分析 任务一：自定义IoC&AOP框架 第一部分 Spring概述 第二部分 核心思想 第三部分 手写实现IOC和AOP

---
+++ spring-transfer/src/main/java/com/ccarlos/edu/service/impl/TransferServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ccarlos.edu.service.impl;
+
+import com.ccarlos.edu.dao.AccountDao;
+import com.ccarlos.edu.dao.impl.JdbcAccountDaoImpl;
+import com.ccarlos.edu.pojo.Account;
+import com.ccarlos.edu.service.TransferService;
+
+public class TransferServiceImpl implements TransferService {
+    private AccountDao accountDao = new JdbcAccountDaoImpl();
+    @Override
+    public void transfer(String fromCardNo, String toCardNo, int money)
+            throws Exception {
+        Account from = accountDao.queryAccountByCardNo(fromCardNo);
+        Account to = accountDao.queryAccountByCardNo(toCardNo);
+        from.setMoney(from.getMoney()-money);
+        to.setMoney(to.getMoney()+money);
+        accountDao.updateAccountByCardNo(from);
+        accountDao.updateAccountByCardNo(to);
+    }
+}
