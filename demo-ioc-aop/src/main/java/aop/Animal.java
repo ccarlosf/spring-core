85beb8b42c2215b26ea96d573a71daa2d19cc5e8
ccarlosf <2238513100@qq.com>
2020-09-18 11:39:58 +0800

    oop

---
+++ demo-ioc-aop/src/main/java/aop/Animal.java
@@ -0,0 +1,29 @@
+package aop;
+
+/**
+ * @author 应癫
+ */
+public class Animal {
+
+    private String height; // 高度
+    private float weight;  // 体重
+
+    public void eat(){
+
+
+
+        // 业务逻辑代码
+        System.out.println("I can eat...");
+
+
+    }
+
+    public void run(){
+
+
+        // 业务逻辑代码·
+        System.out.println("I can run...");
+
+    }
+
+}
