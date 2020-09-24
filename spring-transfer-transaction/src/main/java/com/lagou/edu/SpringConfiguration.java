0add4a046c28ef8fc548f6155b14b2868d46f5d8
ccarlosf <2238513100@qq.com>
2020-09-24 15:51:10 +0800

    01 第一阶段 开源框架源码剖析 模块二 IoC容器设计实现及Spring源码分析 Spring高级框架 第六部分 Spring AOP 应用 第5节 Spring声明式事务的支持

---
+++ spring-transfer-transaction/src/main/java/com/lagou/edu/SpringConfiguration.java
@@ -0,0 +1,7 @@
+package com.lagou.edu;
+
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement//开启spring注解事务的⽀持
+public class SpringConfiguration {
+}
